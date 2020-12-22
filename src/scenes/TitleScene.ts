40ef4e92f64a15490319737c631667a2b5214cf0
erugg22 <41970889+erugg22@users.noreply.github.com>
2020-12-22 14:10:53 -0500

    tried to make a for each loop

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -23,6 +23,7 @@ export default class TitleScene extends Scene {
     private currentColorValue:number = 0;
     private userOrder:Array<number> = [];
     private levels:Array<PIXI.Sprite> = [];
+    private allLevels:Array<PIXI.Sprite> = [];
 
     preload():AssetList {
         return [
@@ -63,6 +64,7 @@ export default class TitleScene extends Scene {
                 this.levels[n].y += n*LEVEL_OFFSET;
                 this.levels[n].alpha = 0;
                 this.addChild(this.levels[n]);
+                this.allLevels.push(this.levels[n]);
             });
         }
 
@@ -92,9 +94,9 @@ export default class TitleScene extends Scene {
 
     start() {
 
-        let n:number = -1;
+        //let n:number = -1;
         let a:number = 0;
-
+        
         // todo: this nested for loop can be rewritten as a single for loop over this.levels[]
         // and we can get rid of n:
         // like this:
@@ -103,14 +105,21 @@ export default class TitleScene extends Scene {
             //
         });
         */
+
+        this.allLevels.forEach = (element, index) => {
+            a = 1-index*0.07;
+            Tween.get(element).wait(100*index).to({alpha:a},a*1200);
+        }
+        /*
         for (let index = 0; index < 5; index++) {
 
             this.colors.forEach(element => {
                 n++;
                 a = 1-n*0.07;
-                Tween.get(this.levels[n]).wait(100*n).to({alpha:a},a*1200);
+                Tween.get(this.allLevels[n]).wait(100*n).to({alpha:a},a*1200);
             });
         }
+        */
 
         this.btnPink.buttonMode = true;
         this.btnPink.interactive = true;
